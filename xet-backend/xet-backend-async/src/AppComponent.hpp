f259b4dd316c9aecd06650b3c36df565dd28fcbd
Aldebaran638 <2023191113@email.szu.edu.cn>
2025-05-23 23:21:14 +0800

    修改连接提供器,监听所有IPV6

--- xet-backend/xet-backend-async/src/AppComponent.hpp
+++ xet-backend/xet-backend-async/src/AppComponent.hpp
@@ -53,7 +53,7 @@ public:
    *  Create ConnectionProvider component which listens on the port
    */
   OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, serverConnectionProvider)([]
-                                                                                                              { return oatpp::network::tcp::server::ConnectionProvider::createShared({"0.0.0.0", 8001, oatpp::network::Address::IP_6}); }());
+                                                                                                              { return oatpp::network::tcp::server::ConnectionProvider::createShared({"::", 8000, oatpp::network::Address::IP_6}); }());
 
   /**
    *  Create Router component
